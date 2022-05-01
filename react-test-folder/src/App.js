890fec041bcc3b0469341e58cd4f3aa3d589cde2
belulu123 <lucianabelu8@gmail.com>
2022-05-01 19:33:03 -0300

    React stuff
    Carpeta de react

---
+++ react-test-folder/src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div>
+      <h1> Olaa</h1>
+      <div>
+      olaa estoy probando
+      <div>
+      <button>Boton</button>
+      </div>
+      </div>
+    </div>
+    
+
+  );
+}
+
+export default App;
