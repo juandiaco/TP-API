7cb5df537eee98710527c6012b5b159640897c1e
belulu123 <lucianabelu8@gmail.com>
2022-05-05 18:28:36 -0300

    arreglos main
    aaa

--- react-test-folder/src/App.js
+++ react-test-folder/src/App.js
@@ -6,6 +6,17 @@ function App() {
       olaa estoy probando
       <div>
       <button>Boton</button>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the bulk of
+          the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
       </div>
       </div>
     </div>
