ef230c9cac1799d845307c5ab0c78ec089034f84
unknown <juancruz1097@gmail.com>
2022-05-25 19:39:44 -0300

    VALIDACION EN FORMS

--- react-test-folder/src/components/JS/Login.js
+++ react-test-folder/src/components/JS/Login.js
@@ -1,30 +1,107 @@
 
-import { Link } from 'react-router-dom';
+import { Link, Navigate} from 'react-router-dom';
 import {Form, FormGroup, Button,} from 'react-bootstrap';
 import { Col, Container, Row } from 'react-bootstrap';
+import { useState, useEffect} from "react";
+
 
 function Login(){
     document.body.style.backgroundColor= "bisque";
+
+    const initialValues = {username:"", email:"", password:"", password2:"", fecha:""};
+    const [formValues, setFormValues] = useState(initialValues)
+    const [formErros, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+        
+    const handleChange = (e) => {
+
+
+        const {name, value} = e.target;
+        setFormValues({...formValues, [name]: value});
+        console.log(formValues);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormErrors(validate(formValues));
+
+        setIsSubmit(true);
+    }
+        
+   
+    useEffect(() =>{
+        console.log(formErros);
+        if (Object.keys(formErros).length === 0 && isSubmit){
+            console.log(formValues);
+        }
+    }, [formErros]);
+
+
+
+    const validate = (values) => {
+
+        const errors = {};
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+        if (!values.username){
+            errors.username = "Ingresa tu nombre";
+        }
+
+        if (!values.email){
+            errors.email = "Ingresa tu correo electrónico";
+        }
+        else if(!regex.test(values.email)){
+            errors.email = "No es un formato válido de correo electrónico";
+        }
+ 
+        if (!values.password){
+            errors.password = "Ingresa tu contraseña";
+        }
+        else if(values.password.length < 4){
+            errors.password = "La contraseña tiene menos de 4 caracteres";
+        }
+
+        if(values.password.localeCompare(values.password2) ){
+            errors.password2= "Las contraseñas no coinciden";
+            console.log("contraseñas no coinciden");
+        }
+
+        if(!values.fecha){
+            errors.fecha = "Ingresa tu fecha de nacimiento";
+        }
+        
+
+        return errors;
+    }
+
+    const handleMain = () => {
+        <Navigate to="/main" replace={true} />
+    }
+
     return (
         <div className='loginc'>
             <div class="containerLogin">
                         <h1 className="container text-center tituloLogin">¡Bienvenido!</h1>
                         <h2 className='container text-center subtituloLogin'> ¿List@ para cocinar? </h2>
                         <div class="container text-center">
-                            <Form>
+                            <Form onSubmit={handleSubmit}>
                                 <Form.Group controlId="formBasicEmail">
                                         <Form.Label className='labelLogin'>Correo Electrónico</Form.Label> 
-                                                <Form.Control className="formcontrol" type="email" placeholder="Ingresa tu correo electrónico" />        
+                                        <Form.Control name="email" className="formcontrolLogin" type="text" placeholder="Ingresa tu correo electrónico" value={formValues.email} onChange={handleChange}/>        
+                                        <p className='errorLogin'>{formErros.email}</p>
                                 </Form.Group>
                                 <Form.Group controlId="formBasicPassword">
                                         <Form.Label className='labelLogin'>Contraseña</Form.Label>
-                                        <Form.Control className="formcontrol" type="password" placeholder="Ingresa tu contraseña" />
+                                        <Form.Control className="formcontrolLogin" type="password" name="password" placeholder="Ingresa tu contraseña" value={formValues.password} onChange={handleChange}/>
+                                        <p className='errorLogin'>{formErros.password}</p>
                                 </Form.Group>
 
+   
                                 <Link class="" to='/main'><button className="btn btn-primary btn-lg btn">¡Listo!</button></Link>
                                 <br/>
                                 <Link class="" to='/RecuperoContraseña'><button className="btn"> Olvidé mi contraseña </button></Link>
                                 
+          
+
                             </Form>
                         </div>
 
--- react-test-folder/src/components/JS/Registrar.js
+++ react-test-folder/src/components/JS/Registrar.js
@@ -1,42 +1,117 @@
 
 import {Form, FormGroup, Button,} from 'react-bootstrap';
-
+import { useState, useEffect} from "react";
 
 function Registrar(){
+    
+    document.body.style.backgroundColor= "bisque";
+
+    const initialValues = {username:"", email:"", password:"", password2:"", fecha:""};
+    const [formValues, setFormValues] = useState(initialValues)
+    const [formErros, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+    
+    const handleChange = (e) => {
+
+
+        const {name, value} = e.target;
+        setFormValues({...formValues, [name]: value});
+        console.log(formValues);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormErrors(validate(formValues));
+
+        setIsSubmit(true);
+    }
+        
+   
+    useEffect(() =>{
+        console.log(formErros);
+        if (Object.keys(formErros).length === 0 && isSubmit){
+            console.log(formValues);
+        }
+    }, [formErros]);
+
+
+    const validate = (values) => {
+
+        const errors = {};
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+        if (!values.username){
+            errors.username = "Ingresa tu nombre";
+        }
+
+        if (!values.email){
+            errors.email = "Ingresa tu correo electrónico";
+        }
+        else if(!regex.test(values.email)){
+            errors.email = "No es un formato válido de correo electrónico";
+        }
+ 
+        if (!values.password){
+            errors.password = "Ingresa tu contraseña";
+        }
+        else if(values.password.length < 4){
+            errors.password = "La contraseña tiene menos de 4 caracteres";
+        }
+
+        if(values.password.localeCompare(values.password2) ){
+            errors.password2= "Las contraseñas no coinciden";
+            console.log("contraseñas no coinciden");
+        }
+
+        if(!values.fecha){
+            errors.fecha = "Ingresa tu fecha de nacimiento";
+        }
+        
+
+        return errors;
+    }
+    
+    
     return (
-        <div>
-            <div > 
-            </div>
-            <div class="container text-center"  >
-                <Form className='inputLogin'>
+
+
+        <div class="container text-center containerLogin">
+            <div class="loginc">
+               
+                <Form onSubmit={handleSubmit}>
                     <div>
-                        <h2 class="text-center">  Creá tu perfil</h2>
+                        <h2 className="text-center">  Creá tu perfil</h2>
                     </div>
-                    <Form.Group>
+                    <Form.Group >
                         <Form.Label>Nombre</Form.Label>
-                        <Form.Control  type="email" placeholder="Ingresa tu nombre" />
+                        <Form.Control className="formcontrolRegistrar" type="text" name="username" placeholder="Ingresa tu nombre" value={formValues.username} onChange={handleChange} />
+                        <p className='errorLogin'>{formErros.username}</p>
+                        
+                        
                 </Form.Group>
 
                 <Form.Group>
                         <Form.Label>Correo Electrónico</Form.Label>
-                        <Form.Control  type="email" placeholder="Ingresa tu correo electrónico" />
+                        <Form.Control className="formcontrolRegistrar" type="text" name="email" placeholder="Ingresa tu correo electrónico" value={formValues.email} onChange={handleChange} />
+                        <p className='errorLogin'>{formErros.email}</p>
                     </Form.Group>
 
                     <Form.Group>
                         <Form.Label>Contraseña</Form.Label>
-                        <Form.Control type="password" placeholder="Ingresa tu contraseña" />
+                        <Form.Control className="formcontrolRegistrar" type="password" name="password" placeholder="Ingresa tu contraseña" value={formValues.password} onChange={handleChange}/>
+                        <p className='errorLogin'>{formErros.password}</p>
                     </Form.Group>
 
                     <Form.Group>
                         <Form.Label>Confirmar contraseña</Form.Label>
-                        <Form.Control  type="password" placeholder="Repetí tu contraseña" />               
+                        <Form.Control className=".formcontrolRegistrar" type="password" name="password2" placeholder="Repetí tu contraseña" value={formValues.password2} onChange={handleChange}/>               
+                        <p className='errorLogin'>{formErros.password2}</p>
                     </Form.Group>
 
 
                     <Form.Group>
                         <Form.Label>Fecha de nacimiento</Form.Label>
-                        <Form.Control type="date" placeholder="Ingresas tu correo electrónico" />
-
+                        <Form.Control type="date" name="fecha" value={formValues.fecha} onChange={handleChange}/>
+                        <p className='errorLogin'>{formErros.fecha}</p>
                     </Form.Group>
                     <br/>
 
@@ -45,10 +120,11 @@ function Registrar(){
                     </Button>
 
                 </Form>
-                </div>
+            </div>
 
-
+                
         </div>
+
        
     );
 }
