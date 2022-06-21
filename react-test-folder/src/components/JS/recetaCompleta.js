9f0db99442a4ebabef584691fdc9efbfbddd8231
belulu123 <lucianabelu8@gmail.com>
2022-06-20 22:02:37 -0300

    prueba
    aaa

--- react-test-folder/src/components/JS/recetaCompleta.js
+++ react-test-folder/src/components/JS/recetaCompleta.js
@@ -1,6 +1,6 @@
 import React from "react";
 import MainNavigation from "./MainNavigation";
-import {Form, FormGroup,Button,Navbar,NavDropdown,Nav,Container, Card, Row, Col,ListGroup,ListGroupItem, Dropdown, DropdownButton,Tab,Tabs} from 'react-bootstrap';
+import {Form, Accordion, AccordionButton, AccordionCollapse, FormGroup,Button,Navbar,NavDropdown,Nav,Container, Card, Row, Col,ListGroup,ListGroupItem, Dropdown, DropdownButton,Tab,Tabs} from 'react-bootstrap';
 
 
 function RecetaCompleta(){
@@ -10,34 +10,42 @@ function RecetaCompleta(){
             <div>
             <MainNavigation/>
             </div>
-            <div className="container">
-            <h1 className="tituloReceta"> Pollo con Papas</h1>
-            <h4 className="tituloReceta"> Chefcito: Luciana Souto </h4>
             
-            <h5>Categoría: Cena</h5>
-            
-            <h5>Ingredientes:</h5>
-            <br/>
-            <li> 1 Pollo trozado </li>
-            <li> 4 Dientes de Ajo </li>
-            <li> 200cc de Vino Blanco </li>
-            <li> Sal, pimienta y laurel, a gusto </li>
-            <li> 4 Papas chicas </li>
-            <br/>
-            <h5>Tiempo estimado: 2 Horas</h5>
-            <br/>
-            <h5>Procedimiento:</h5>
-            <text className="cuerpo">
-                <li>En una cacerola poner un chorrito de aceite de oliva y dorar las piezas de pollo. Reservar a un costado.</li>
-                <li>En la misma cacerola agregar un poquito más de aceite, de ser necesario, y agregar todas las verduras (excepto el tomate y la papa) cortados en juliana.</li>
-                <li>Una vez que la cebolla esté transparente, sumar el tomate triturado o picado bien fino, las piezas de pollo y condimentar. Añadir las hojas de laurel.</li>
-                <li>Sumar el vasito de vino blanco y cocinar hasta que ya no se sienta el olor a alcohol. Agregar caldo hasta cubrir y sumar las papas peladas y cortadas en cubos grandes. Tapar y bajar el fuego.</li>
-                <li>Cocinar aproximadamente por 30 minutos. Hasta que se pinche el pollo y salga líquido transparente y las papas estén tiernas.</li>
-                <li>Rectificar los sabores, agregar las arvejas y dejar reposando un rato antes de servir.</li>
-             
-            </text>
-
-            </div>
+            <Card className="cardReceta">
+                <Card.Header>
+                    <h1>
+                        Nombre de la Receta
+                    </h1>
+                    <h3>
+                        By: Chefcito
+                    </h3>
+                </Card.Header>
+                <Card.Body>
+                    Categoría: ejemplo, ejemplo2
+                    <br/>
+                    Dificultad: Avanzado
+                    <Accordion className="accordion">
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>
+                                <ListGroup>
+                                    
+                                </ListGroup>
+                            </Accordion.Header>
+                                <Accordion.Body> aa </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="1">
+                            <Accordion.Header>Procedimiento</Accordion.Header>
+                            <Accordion.Body>
+                                AA
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
+                </Card.Body>
+                <Card.Footer>
+                    <button className="btn"> Calificar </button>
+                </Card.Footer>
+                
+            </Card>
 
         </div>
 
